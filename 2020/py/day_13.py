e0689595537f92bd98f6c0a30e88167841f0ef39
Ryan Lowe <rlowe13@me.com>
2020-12-14 20:10:48 -0500

    Add 2020 13-14 in Python

---
+++ 2020/py/day_13.py
@@ -0,0 +1,25 @@
+from math import lcm
+from pathlib import Path
+
+
+def main():
+    text = (Path(__file__).parent / "../input/input_13.txt").read_text()
+    # Part 1
+    start, timeline = text.splitlines()
+    start = int(start)
+    bus_data = {int(b): i for i, b in enumerate(timeline.split(',')) if b != 'x'}
+    time_map = {b: start - start % b + b for b in bus_data}
+    first = min(time_map, key=time_map.get)
+    print("Part 1:", first * (time_map[first] - start))
+    # Part 2
+    time = 0
+    period = 1
+    for bus, offset in bus_data.items():
+        while (time + offset) % bus != 0:
+            time += period
+        period = lcm(period, bus)
+    print("Part 2:", time)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2020/py/day_14.py
@@ -0,0 +1,51 @@
+import itertools as it
+from pathlib import Path
+from typing import Callable
+
+
+def write_1(mem, addr, val, mask):
+    for i, b in enumerate(reversed(mask)):
+        if b == '0':
+            val &= ~(1 << i)
+        elif b == '1':
+            val |= 1 << i
+    mem[addr] = val
+
+
+def write_2(mem, addr, val, mask):
+    x_indices = []
+    for i, b in enumerate(reversed(mask)):
+        if b == 'X':
+            x_indices.append(i)
+        elif b == '1':
+            addr |= 1 << i
+    for combo in it.product((0, 1), repeat=len(x_indices)):
+        for i, b in zip(x_indices, combo):
+            if b == 1:
+                addr |= 1 << i
+            else:
+                addr &= ~(1 << i)
+        mem[addr] = val
+
+
+def run(program: list, write_fn: Callable) -> int:
+    mem = {}
+    for line in program:
+        op, val = line.split(' = ')
+        if op == 'mask':
+            mask = val
+        else:
+            addr = int(op[4:-1])
+            write_fn(mem, addr, int(val), mask)
+    return sum(mem.values())
+
+
+def main():
+    text = (Path(__file__).parent / "../input/input_14.txt").read_text()
+    program = text.splitlines()
+    print("Part 1:", run(program, write_1))
+    print("Part 2:", run(program, write_2))
+
+
+if __name__ == "__main__":
+    main()
