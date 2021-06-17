c675e75f23c49666165200edb0b323eb42d360f4
Ryan Lowe <rlowe13@me.com>
2021-06-17 14:13:25 -0600

    feat: add 2019.01 in Rust

---
+++ 2019/rs/src/bin/day_01.rs
@@ -0,0 +1,17 @@
+use std::fs;
+
+fn main() {
+    let text = fs::read_to_string("../input/input_01.txt").unwrap();
+    let module_fuel: i32 = text.lines().map(|line| line.parse::<i32>().unwrap() / 3 - 2).sum();
+    println!("Part 1: {}", module_fuel);
+    let total_fuel: i32 = text.lines().map(|line| {
+        let mut total = 0;
+        let mut mass = line.parse::<i32>().unwrap() / 3 - 2;
+        while mass > 0 {
+            total += mass;
+            mass = mass / 3 - 2;
+        }
+        total
+    }).sum();
+    println!("Part 2: {}", total_fuel);
+}
