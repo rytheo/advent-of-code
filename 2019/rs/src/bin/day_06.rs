eef1583a8f6ba31ab804d1ed96426cbf3c769aef
Ryan Lowe <rlowe13@me.com>
2021-06-21 14:36:41 -0600

    feat: add 2019.6 in Rust

---
+++ 2019/rs/src/bin/day_06.rs
@@ -0,0 +1,27 @@
+use std::collections::{HashMap, HashSet};
+use std::fs;
+
+fn main() {
+    let text = fs::read_to_string("../input/input_06.txt").unwrap();
+    let orbits: HashMap<_, _> = text.lines().map(|line| {
+        let (parent, child) = line.split_once(')').unwrap();
+        (child, parent)
+    }).collect();
+    let mut total = 0;
+    for mut body in orbits.keys() {
+        while *body != "COM" {
+            total += 1;
+            body = &orbits[body];
+        }
+    }
+    println!("Part 1: {}", total);
+    let paths: Vec<_> = ["YOU", "SAN"].iter().map(|mut body| {
+        let mut path: HashSet<_> = HashSet::new();
+        while *body != "COM" {
+            body = &orbits[body];
+            path.insert(body);
+        }
+        path
+    }).collect();
+    println!("Part 2: {}", paths[0].symmetric_difference(&paths[1]).count());
+}
