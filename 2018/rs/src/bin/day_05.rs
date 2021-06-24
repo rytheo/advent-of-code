84a0d67cc32d576c317d7f9b9d53ffbc5ab4967b
Ryan Lowe <rlowe13@me.com>
2021-06-24 16:08:04 -0600

    feat: add 2018.5 in Rust

---
+++ 2018/rs/src/bin/day_05.rs
@@ -0,0 +1,27 @@
+use std::fs;
+
+fn react(bytes: &[u8], remove: Option<u8>) -> usize {
+    let mut stack = Vec::with_capacity(bytes.len());
+    for b in bytes {
+        // Skip letters to be removed
+        if let Some(r) = remove {
+            if b.to_ascii_lowercase() == r.to_ascii_lowercase() { continue; }
+        }
+        // Destroy adjacent letters of opposite case
+        if let Some(a) = stack.last() {
+            if a ^ b == 32 {
+                stack.pop();
+                continue;
+            }
+        }
+        stack.push(*b);
+    }
+    stack.len()
+}
+
+fn main() {
+    let text = fs::read_to_string("../input/input_05.txt").unwrap();
+    let bytes = text.trim().as_bytes();
+    println!("Part 1: {}", react(&bytes, None));
+    println!("Part 2: {}", (b'a'..=b'z').map(|r| react(&bytes, Some(r))).min().unwrap());
+}
