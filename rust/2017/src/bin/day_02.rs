b7449e5e153ecd58f5d3494bc799abf4e08e43d0
Ryan Lowe <rytheo@users.noreply.github.com>
2022-08-07 18:57:21 -0400

    feat: add 2017.02 in Rust

---
+++ rust/2017/src/bin/day_02.rs
@@ -0,0 +1,18 @@
+use std::fs;
+use itertools::Itertools;
+
+fn main() {
+    let text = fs::read_to_string("../input/2017/input_02.txt").unwrap();
+    let mut checksums = [0, 0];
+    for line in text.lines() {
+        let values: Vec<u32> = line.split('\t').filter_map(|s| s.parse().ok()).collect();
+        checksums[0] += values.iter().max().unwrap() - values.iter().min().unwrap();
+        checksums[1] += values.iter().permutations(2).find_map(|p| {
+            let (a, b) = (p[0], p[1]);
+            (a % b == 0).then_some(a / b)
+        }).unwrap();
+    }
+    for (n, checksum) in (1..).zip(checksums) {
+        println!("Part {n}: {checksum}");
+    }
+}
