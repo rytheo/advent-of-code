8794bff0a22ba7007c30a08480dc8ef9e001af05
Ryan Lowe <rytheo@users.noreply.github.com>
2022-10-01 21:09:15 -0400

    feat: add 2017.04-05 in Rust

---
+++ rust/2017/src/bin/day_04.rs
@@ -0,0 +1,19 @@
+use std::fs;
+use itertools::Itertools;
+use aoc::counter::Counter;
+
+fn validate(s: &str, anagrams_ok: bool) -> bool {
+    let counter: Counter<_> = s.split_whitespace().map(|w| match anagrams_ok {
+        true => w.to_owned(),
+        false => w.chars().sorted().collect::<String>()
+    }).collect();
+    counter.values().all(|&v| v < 2)
+}
+
+fn main() {
+    let input = fs::read_to_string("../input/2017/input_04.txt").unwrap();
+    for (i, anagrams_ok) in [(1, true), (2, false)] {
+        let num_valid = input.lines().filter(|s| validate(s, anagrams_ok)).count();
+        println!("Part {i}: {num_valid}");
+    }
+}
---
+++ rust/2017/src/bin/day_05.rs
@@ -0,0 +1,25 @@
+use std::fs;
+
+fn run(offsets: &[isize], strange: bool) -> usize {
+    let mut offsets = offsets.to_owned();
+    let mut position = 0isize;
+    let mut steps = 0;
+    while 0 <= position && (position as usize) < offsets.len() {
+        let offset = offsets[position as usize];
+        let delta = if strange && offset >= 3 { -1 } else { 1 };
+        offsets[position as usize] += delta;
+        position += offset;
+        steps += 1;
+    }
+    steps
+}
+
+fn main() {
+    let input = fs::read_to_string("../input/2017/input_05.txt").unwrap();
+    let offsets = input.lines()
+        .map(|s| s.parse().unwrap())
+        .collect::<Vec<isize>>();
+    for (i, strange) in [(1, false), (2, true)] {
+        println!("Part {i}: {}", run(&offsets, strange));
+    }
+}
