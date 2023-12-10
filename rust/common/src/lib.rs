dfcdd83f721ae0ba1e6e7061a09c1b20dd1d7554
Ryan Lowe <rytheo@users.noreply.github.com>
2023-12-09 20:08:05 -0500

    feat(rust): add aoc::Part

--- rust/common/src/lib.rs
+++ rust/common/src/lib.rs
@@ -1,7 +1,20 @@
+use std::fmt;
 use std::path::{Path, PathBuf};
 
 pub mod counter;
 pub mod geometry;
+
+#[derive(Clone, Copy, Debug)]
+pub enum Part {
+    One = 1,
+    Two = 2,
+}
+
+impl fmt::Display for Part {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Part {}", *self as u8)
+    }
+}
 
 pub fn read_input(path: &str) -> String {
     let manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
